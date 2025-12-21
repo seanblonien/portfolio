eab493a2a468c38fe823ea39b6067ed9053cf8c8
Sean Blonien <seanb2016@gmail.com>
2025-12-20 21:45:54 -0600

    fix: Package updates

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
-import Analyzer from '@next/bundle-analyzer';
 import type { NextConfig } from 'next';
 
-const withBundleAnalyzer = Analyzer({
-  enabled: process.env.ANALYZE === 'true',
-});
-
-const nextConfig: NextConfig = withBundleAnalyzer({
+const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -25,6 +20,6 @@ const nextConfig: NextConfig = withBundleAnalyzer({
     optimizeCss: true,
     cssChunking: true,
   },
-});
+};
 
 export default nextConfig;
