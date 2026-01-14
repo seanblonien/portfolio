a8cfb151fe454f097f8d1c0be44572083f1eb254
Sean Blonien <seanb2016@gmail.com>
2026-01-14 17:18:30 -0600

    fix: Tailwind v4 upgrade

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    '@tailwindcss/postcss': {},
+    'autoprefixer': {},
     ...process.env.NODE_ENV === 'production' ? { cssnano: {} } : {},
   },
 };
